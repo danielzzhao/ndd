250cfa261b3f9cf937bbb62132c0f2c48100756b
danielzzhao@gmail.com <danielzzhao@gmail.com>
2015-07-23 13:34:57 +0200

    seed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,29 @@
 class TasksController < ApplicationController
-	def index
 
-		@tasks = city.tasks
+	def index
+		@city = City.find(params[:city_id])
+		@tasks = @city.tasks
 	end
 
-	# private
-	# def task_params
-	# 	params.require(:task).permit(:name, :description, :complete)
-	# end
+	def show
+		@tasks = cit
+	end
+
+	def new
+		@task = Tasks.new
+	end
+
+	def create
+		@task = tasks.build(task_params)
+		if @task.save
+			redirect_to cities_tasks_path(@task)
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def task_params
+		params.require(:task).permit(:name, :description, :complete, :image, :dreamer)
+	end
 end
